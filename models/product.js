f5852a3070866d4a12e94d0c41f4dec98474718b
NPVinodya <prabanivinodya@gmail.com>
2024-12-17 00:35:06 +0530

    create productSchema

--- models/product.js
+++ models/product.js
@@ -1,9 +1,41 @@
-import mongoose from "mongoose";
+import mongoose, { trusted } from "mongoose";
 
 const productSchema = mongoose.Schema({
-    name:String,
-    price:Number,
-    description:String 
+    productId:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    productName:{
+        type:String,
+        required:true
+    },
+    altNames:[
+        {
+            type:String
+        }
+    ],
+    images:[
+        {
+            type:String
+        }
+    ],
+    price:{
+        type:Number,
+        required:true
+    },
+    lastPrice:{
+        type:Number,
+        required:true
+    },
+    stock:{
+        type:Number,
+        requierd:true
+    },
+    description:{
+        type:String,
+        required:true
+    } 
 })
 const Product = mongoose.model("products",productSchema)
 
